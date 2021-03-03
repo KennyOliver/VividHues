7c1b1c72d267c5393ce8a8f23447a431c5fdc34e
Kenny <kenny_oliver@icloud.com>
2021-03-03 08:34:52 +0000

    [+] Setup Bash and vividHues.py [+] Added class with colours

---
+++ vividHues.py
@@ -0,0 +1,17 @@
+import os
+os.system("") #allows colours to be displayed in the console
+
+class Clr:
+  BLACK = "\033[30m"
+  RED = "\033[31m"
+  GREEN = "\033[32m"
+  YELLOW = "\033[33m"
+  BLUE = "\033[34m"
+  MAGENTA = "\033[35m"
+  CYAN = "\033[36m"
+  WHITE = "\033[37m"
+  UNDERLINE = "\033[4m"
+  RESET = "\033[0m"
+
+print(Clr.RED, "Hi there!" ,Clr.RESET)
+print("HI")
