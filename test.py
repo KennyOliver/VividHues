69848ff6ed7e2463d56c99eadeb0fd2460e816cf
Kenny <70860732+KennyOliver@users.noreply.github.com>
2021-03-14 19:25:12 +0000

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+import unittest
+from VividHues import __init__
+#====================
+def test_VividHues():
+  print(Clr.YELLOW + "If you see this..." + Clr.GREEN + "VividHues works!!!" + Clr.RESET)
+#====================
+# MAIN PROGRAM
+
+test_VividHues()
