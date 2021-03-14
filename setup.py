df4209d360ea7eef6d5eae0f334dc0b88d676e09
Kenny <70860732+KennyOliver@users.noreply.github.com>
2021-03-14 16:29:16 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from setuptools import setup
 
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
-    name='vividHues',
-    url='https://github.com/KennyOliver/vivid-hues',
+    name='VividHues',
+    url='https://github.com/KennyOliver/VividHues',
     author='Kenneth Oliver',
     author_email='kenny_oliver@icloud.com',
     # Needed to actually package something
-    packages=['vividHues'],
+    packages=['VividHues'],
     # Needed for dependencies
     #install_requires=['numpy'],
     # *strongly* suggested for sharing
     version='2.5.1',
     # The license can be anything you like
     license='AGPL',
-    description='Lightweight Python module for coloured strings in the Python console',
+    description='VividHues is a lightweight Python module for coloured strings in the Python console!',
     # We will also need a readme eventually (there will be a warning)
     # long_description=open('README.txt').read(),
 )
