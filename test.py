cfd2e6d37e3fd1ba97577a037987f74af59c3d61
Kenny <70860732+KennyOliver@users.noreply.github.com>
2021-03-14 19:28:01 +0000

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from VividHues import __init__
-#====================
-def test_VividHues():
-  print(Clr.YELLOW + "If you see this..." + Clr.GREEN + "VividHues works!!!" + Clr.RESET)
-#====================
-# MAIN PROGRAM
-
-test_VividHues()
