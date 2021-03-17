fb224e958972130e87d3e90c68c81a9b9e4c8b80
Kenny <70860732+KennyOliver@users.noreply.github.com>
2021-03-17 19:38:17 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup
 
+print("\033[93m" + "Installing VividHues..." + "\033[0m")
+
 setup(
     name='VividHues', #needed to silence warnings (and to be a worthwhile package)
     url='https://github.com/KennyOliver/VividHues',
@@ -12,3 +14,5 @@ setup(
     description='VividHues is a lightweight Python package for coloured strings in the Python console!',
     # long_description=open('README.txt').read(), #you need a readme eventually (there will be a warning)
 )
+
+print("\033[32m" + "\033[1m" + "Success!!! Have colourful fun! ;D" + "\033[0m")
