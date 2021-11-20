f429c588c36727a7ba12196d76c979cf71a2424e
Kenny <70860732+KennyOliver@users.noreply.github.com>
2021-11-20 20:24:04 +0000

    Update __init__.py

--- VividHues/__init__.py
+++ VividHues/__init__.py
@@ -1,18 +1,18 @@
 import os
-os.system("") #allows colours to be displayed in the console
+os.system("")  # allows colours to be displayed in the console
 
 class Clr:
-  BLACK = "\033[30m"
-  RED = "\033[31m"
-  GREEN = "\033[32m"
-  ORANGE = "\033[33m"
-  BLUE = "\033[34m"
-  PURPLE = "\033[35m"
-  CYAN = "\033[36m"
-  WHITE = "\033[37m"
-  LIME = "\033[92m"
-  YELLOW = "\033[93m"
-  PINK = "\033[95m"
-  UNDERLINE = "\033[4m"
-  BOLD = "\033[01m"
-  RESET = "\033[0m"
+    BLACK = "\033[30m"
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    ORANGE = "\033[33m"
+    BLUE = "\033[34m"
+    PURPLE = "\033[35m"
+    CYAN = "\033[36m"
+    WHITE = "\033[37m"
+    LIME = "\033[92m"
+    YELLOW = "\033[93m"
+    PINK = "\033[95m"
+    UNDERLINE = "\033[4m"
+    BOLD = "\033[01m"
+    RESET = "\033[0m"
