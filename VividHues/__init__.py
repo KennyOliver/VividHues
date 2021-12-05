44781681c6645577dac89cadccca7dd2a586dc72
esouidi <esouidi@enssat.fr>
2021-12-06 00:08:24 +0100

    Revert "Use Random to get Colors"
    This reverts commit 97bdc86d35329bea531c012551efc29cc33cd8f2.

--- VividHues/__init__.py
+++ VividHues/__init__.py
@@ -1,6 +1,4 @@
 import os
-import random
-
 os.system("")  # allows colours to be displayed in the console
 
 
@@ -16,31 +14,10 @@ class Clr:
     LIME = "\033[92m"
     YELLOW = "\033[93m"
     PINK = "\033[95m"
-
+    
     UNDERLINE = "\033[4m"
     UL = UNDERLINE
     BOLD = "\033[01m"
     BO = BOLD
     RESET = "\033[0m"
     RS = RESET
-
-    @classmethod
-    def random_color(cls):
-        colors = []  # List of all the colors in the class Clr
-        for key in Clr.__dict__.keys():
-            if key == "UNDERLINE":  # Once we got to UNDERLINE, there is no more color
-                break
-            else:
-                colors.append(Clr.__dict__[key])
-        colors.pop(0)  # Remove the first key of (Clr.__dict__.keys()). Not a color
-        return colors[random.randint(0, len(colors) - 1)]
-
-
-def print_string_rainbow_colors(string_to_print):
-    for letter in string_to_print:
-        print(f'{Clr.random_color()}' + letter, end="")
-    print("")
-
-
-def print_string_random_color(string_to_print):
-    print(f'{Clr.random_color()}' + string_to_print)
