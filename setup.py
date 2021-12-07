7250e12c2850e4e00730b576bad71863fc87b463
Kenny <70860732+KennyOliver@users.noreply.github.com>
2021-12-07 22:31:25 +0000

    Cleaning up setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,16 @@ from setuptools import setup
 print("\033[93m" + "Installing VividHues..." + "\033[0m")
 
 setup(
-    name='VividHues',  # needed to silence warnings (and to be a worthwhile package)
+    name='VividHues',
     url='https://github.com/KennyOliver/VividHues',
     author='Kenneth Oliver',
     author_email='kenny_oliver@icloud.com',
-    packages=['VividHues'],  # needed to actually package something
-    # install_requires=['numpy'], #needed for dependencies
+    packages=['VividHues'],
     version='4.1.0',
-    license='AGPL',  # license can be anything you like
+    license='AGPL-v3',
     description='VividHues: super lightweight package for coloured strings in Python!',
-    long_description=open('README.md').read(),  # you need a readme eventually (there will be a warning)
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
 )
 
-print("\033[32m" + "\033[1m" + "Success!!! Have colourful fun! ;D" + "\033[0m")
+print("\033[32m" + "\033[1m" + "VividHues was installed successfully! Have colorful fun! :D" + "\033[0m")
