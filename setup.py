1bc274db6672d88419e69bfa559951d4257694a6
Kenny <70860732+KennyOliver@users.noreply.github.com>
2021-12-10 21:48:47 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
 
+"""
+Setup file for VividHues
+"""
+
 import codecs
 import os.path
+
 
 def read(rel_path):
     here = os.path.abspath(os.path.dirname(__file__))
     with codecs.open(os.path.join(here, rel_path), 'r') as fp:
         return fp.read()
+
 
 def get_version(rel_path):
     for line in read(rel_path).splitlines():
@@ -16,10 +22,9 @@ def get_version(rel_path):
     else:
         raise RuntimeError("Unable to find version string.")
 
+
 NAME = "VividHues"
-
 print("\033[93m" + f"Installing {NAME}..." + "\033[0m")
-
 setup(
     name="VividHues",
     url=f"https://github.com/KennyOliver/{NAME}",
@@ -27,10 +32,9 @@ setup(
     author_email="kenny_oliver@icloud.com",
     packages=[NAME],
     version=get_version("package/__init__.py"),
-    license="AGPL-v3",
+    license="AGPL-3.0",
     description=f"{NAME}: super lite package for colored strings in Python!",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
 )
-
 print("\033[32m" + "\033[1m" + f"{NAME} was installed successfully! Have colorful fun! :D" + "\033[0m")
