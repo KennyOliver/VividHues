c054fbd1c6f92cd3976a690dabdfe2355736a224
Kenny <70860732+KennyOliver@users.noreply.github.com>
2022-02-23 16:26:20 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     author="Kenneth Oliver",
     author_email="kenny_oliver@icloud.com",
     packages=[NAME],
-    version="5.2.7",
+    version="5.2.8",
     license="AGPL-3.0",
     description=f"{NAME}: super lite package for colored strings in Python!",
     long_description=open("README.md").read(),
